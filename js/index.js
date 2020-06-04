d7c27de4d8824244603c9523db1a371b32f4d62e
chook-igor <igor_chuyko@hotmail.com>
2020-06-04 21:17:21 +0000

    Added some features

--- js/index.js
+++ js/index.js
@@ -1,21 +1,55 @@
 const display = document.querySelector('.display');
-
+let sign = '';
+let memory = 0;
 document.querySelectorAll('.numerals button')
   .forEach(button => button.addEventListener('click', digitPressed));
-
+  
   function digitPressed(ev) {
+    
     display.value += ev.target.innerText;
   }
 
-  document.querySelectorAll('.oper button')
+document.querySelectorAll('.oper button')
   .forEach(button => button.addEventListener('click', operPressed));
 
-  function operPressed(ev) {
-    display.value += ev.target.innerText;
-  }
+  /*function operPressed(ev) {
+    if (sign === '') {
+      sign = ev.target.innerText
+      display.value += sign;
+    } else {
+       sign = sign.slice(0, -1)
+        
+    }
+  }*/
 
-  document.querySelector('.equal').addEventListener('click', calculate);
+document.querySelector('.equal button').addEventListener('click', calculate);
 
   function calculate() {
       display.value = eval(display.value);
+  }
+
+document.querySelector('.func .clear').addEventListener('click', clear);
+
+  function clear() {
+      display.value = '';
+  }
+
+document.querySelector('.func .mem-in').addEventListener('click', memAdd);
+
+  function memAdd() {
+    display.value = eval(display.value);
+    memory = parseFloat(display.value);
+  }
+
+document.querySelector('.func .mem-out').addEventListener('click', memOut);
+
+  function memOut() {
+    display.value += memory.toString();
+  }
+
+  document.querySelector('.func .mem-clr').addEventListener('click', memClear);
+
+  function memClear() {
+    memory = '';
+    
   }
