db1e4889a59c550d5cd3ed9ba395229ca73ab606
chook-igor <igor_chuyko@hotmail.com>
2020-06-05 08:11:49 +0000

    Almost finished

--- js/index.js
+++ js/index.js
@@ -1,55 +1,82 @@
-const display = document.querySelector('.display');
-let sign = '';
-let memory = 0;
-document.querySelectorAll('.numerals button')
-  .forEach(button => button.addEventListener('click', digitPressed));
-  
-  function digitPressed(ev) {
-    
-    display.value += ev.target.innerText;
+const display = document.querySelector(".display");
+let memory = '';
+
+document
+  .querySelectorAll(".numerals button")
+  .forEach((button) => button.addEventListener("click", digitPressed));
+
+function digitPressed(ev) {
+  display.value += ev.target.innerText;
+}
+
+document
+  .querySelectorAll(".oper-top button")
+  .forEach((button) => button.addEventListener("click", operTopPressed));
+
+function operTopPressed(ev) {
+  if (
+    display.value.endsWith(".") ||
+    display.value.endsWith("(") ||
+    display.value.endsWith(")")
+     )
+    display.value = display.value.slice(0, -1);
+  display.value += ev.target.innerText;
+}
+
+document
+  .querySelectorAll(".oper button")
+  .forEach((button) => button.addEventListener("click", operPressed));
+
+function operPressed(ev) {
+  if (
+    display.value.endsWith("+") ||
+    display.value.endsWith("-") ||
+    display.value.endsWith("*") ||
+    display.value.endsWith("/")
+  )
+    display.value = display.value.slice(0, -1);
+  display.value += ev.target.innerText;
+}
+
+document.querySelector(".equal button").addEventListener("click", calculate);
+
+function calculate() {
+  if (display.value.endsWith("/0")) {
+    alert("You can't do that");
+  } else if (display.value === "") {
+    alert("42");
   }
+  display.value = eval(display.value);
+   if (display.value === 'undefined' || display.value === 'Infinity') display.value = '';
+}
 
-document.querySelectorAll('.oper button')
-  .forEach(button => button.addEventListener('click', operPressed));
+document.querySelector(".func .clear").addEventListener("click", clear);
 
-  /*function operPressed(ev) {
-    if (sign === '') {
-      sign = ev.target.innerText
-      display.value += sign;
-    } else {
-       sign = sign.slice(0, -1)
-        
-    }
-  }*/
+function clear() {
+  display.value = "";
+}
 
-document.querySelector('.equal button').addEventListener('click', calculate);
+document.querySelector(".func .clear-last").addEventListener("click", clearLst);
 
-  function calculate() {
-      display.value = eval(display.value);
-  }
+function clearLst() {
+  display.value = display.value.slice(0, -1);
+}
 
-document.querySelector('.func .clear').addEventListener('click', clear);
+document.querySelector(".func .mem-in").addEventListener("click", memAdd);
 
-  function clear() {
-      display.value = '';
-  }
+function memAdd() {
+  display.value = eval(display.value);
+  memory = parseFloat(display.value);
+}
 
-document.querySelector('.func .mem-in').addEventListener('click', memAdd);
+document.querySelector(".func .mem-out").addEventListener("click", memOut);
 
-  function memAdd() {
-    display.value = eval(display.value);
-    memory = parseFloat(display.value);
-  }
+function memOut() {
+  display.value += memory.toString();
+}
 
-document.querySelector('.func .mem-out').addEventListener('click', memOut);
+document.querySelector(".func .mem-clr").addEventListener("click", memClear);
 
-  function memOut() {
-    display.value += memory.toString();
-  }
-
-  document.querySelector('.func .mem-clr').addEventListener('click', memClear);
-
-  function memClear() {
-    memory = '';
-    
-  }
+function memClear() {
+  memory = "";
+}
